a266153b83e2a0668848535218cbdbccd7607183
Adarsh Murthy <am4417@columbia.edu>
2019-06-15 16:00:19 -0400

    Update utils file to make the token optional.

--- github_dashboard/backend/utils.py
+++ github_dashboard/backend/utils.py
@@ -5,15 +5,17 @@ import requests
 GITHUB_API = "https://api.github.com"
 GITHUB_API_KEY = os.environ.get("GITHUB_API_KEY")
 
-# Headers to make calls to github
-HEADERS = {"Authorization": "Token {}".format(GITHUB_API_KEY)}
+headers = {}
+if GITHUB_API_KEY:
+    # Headers to make calls to github
+    headers = {"Authorization": "Token {}".format(GITHUB_API_KEY)}
 
 
 def get_repositories(organization):
     """Get repositories from github for an organization."""
     # Construct the url.
     url = "{}/orgs/{}/repos".format(GITHUB_API, organization)
-    response = requests.get(url, headers=HEADERS)
+    response = requests.get(url, headers=headers)
     if not response.status_code == 200:
         raise ValueError(response.json().get("message"))
     content = response.json()
@@ -34,6 +36,6 @@ def update_repos_with_contributor_count(repos):
 def update_repo_with_contributor_count(repo):
     """Given a repository, update it to have the number of contributors."""
     contributors_url = repo.get("contributors_url")
-    response = requests.get(contributors_url, headers=HEADERS)
+    response = requests.get(contributors_url, headers=headers)
     repo["contributors_count"] = len(response.json())
     return repo
